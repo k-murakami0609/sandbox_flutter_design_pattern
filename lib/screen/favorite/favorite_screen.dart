5e0801bbe1bf9e0c6ec08ba0ea8bb1d302ed7933
k-murakami0609 <dadgac@gmail.com>
2019-07-16 23:39:04 +0900

    お気に入り画面を作成

--- lib/screen/favorite/favorite_screen.dart
+++ lib/screen/favorite/favorite_screen.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_design_pattern/screen/repository/repository_view_screen.dart';
+
+class UserRepositoryData {
+  String _repositoryName;
+  String _url;
+
+  UserRepositoryData(this._repositoryName, this._url);
+
+  String get repositoryName => _repositoryName;
+
+  set repositoryName(String value) {
+    _repositoryName = value;
+  }
+
+  String get url => _url;
+
+  set url(String value) {
+    _url = value;
+  }
+}
 
 class FavoriteScreen extends StatelessWidget {
+  final List<UserRepositoryData> users = [
+    UserRepositoryData("repo1", "https://www.google.com"),
+    UserRepositoryData("repo2", "https://www.google.com"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Route'),
+        title: Text('検索 Route'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('元の場所へ route')
-        ),
-      ),
+      body: ListView(
+          children: users
+              .map((user) => Card(
+                  child: ListTile(
+                      leading: FlutterLogo(size: 72.0),
+                      title: Text(user._repositoryName),
+                      subtitle: Text(user._url),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RepositoryViewScreen()),
+                        );
+                      })))
+              .toList()),
     );
   }
 }
